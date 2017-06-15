c7fcc606754154f8c27a653cf343e1ffc67241cc
prakash-ces <prakash.perumalmariappan@cesltd.com>
2017-06-15 19:05:14 +0530

    yo angular complete but with errors

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transupportgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the transupportgithubioApp
+ */
+angular.module('transupportgithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transupportgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the transupportgithubioApp
+ */
+angular.module('transupportgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
